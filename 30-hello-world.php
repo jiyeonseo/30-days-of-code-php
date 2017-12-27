94a12fea741c44ae08e6671f927e47a6eb2f7c88
Jiyeon Seo <jiyeonseo@fb.com>
2017-12-27 02:37:50 -0800

    solve day 1 problem

---
+++ 30-hello-world.php
@@ -0,0 +1,15 @@
+<?php
+// https://www.hackerrank.com/challenges/30-hello-world/problem
+
+$_fp = fopen("php://stdin", "r");
+
+$inputString = fgets($_fp); // get a line of input from stdin and save it to our variable
+
+// Your first line of output goes here
+print("Hello, World.\n");
+
+// Write the second line of output
+print($inputString);
+
+fclose($_fp);
+?>
